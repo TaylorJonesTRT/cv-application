056339d8df0ed5690a5514c86e73e1b10c8c104d
Taylor Jones <taylor@taylorwjones.com>
2021-01-06 21:54:46 -0600

    Started on Personal section with inputs and styling and some logic for updating values in state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
 import './styles/App.css';
+import Header from './components/Header';
+import Personal from './components/Personal';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Personal />
       </div>
     )
   }
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -3,13 +3,62 @@ import React, { Component } from 'react';
 class Personal extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+    }
+  }
+
+  updateValue = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
     return (
-      <div>
-        Hello!
-        <h1>{this.props.id}</h1>
+      <div className='personal-section'>
+        <h1>Personal Information</h1>
+        <hr />
+        <form className='personal-form'>
+          <label>
+            <input
+              type='text'
+              placeholder='First Name'
+              value={this.state.firstName}
+              className='first-name'
+              name='firstName'
+              onChange={this.updateValue} />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Last Name'
+              value={this.state.lastName}
+              className='last-name'
+              name='lastName'
+              onChange={this.updateValue} />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Email'
+              value={this.state.email}
+              className='email'
+              name='email'
+              onChange={this.updateValue} />
+          </label>
+          <label>
+            <input
+              type='text'
+              placeholder='Phone Number'
+              value={this.state.phoneNumber}
+              className='phone-number'
+              name='phoneNumber'
+              onChange={this.updateValue} />
+          </label>
+        </form>
       </div>
     )
   }
