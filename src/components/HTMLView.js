5140c01fc3dfcadd70675490c3b04a5b1bf3d016
Taylor Jones <taylor@taylorwjones.com>
2021-01-15 19:56:22 -0600

    Finished HTML view component

--- src/components/HTMLView.js
+++ src/components/HTMLView.js
@@ -9,7 +9,30 @@ class HTMLView extends Component {
     return this.props.editView ? (
       null
     ) : (
-        <div>hi</div>
+        <div>
+          <div className='preview section'>
+            <h1>Personal Information</h1>
+            <hr />
+            <p>{this.props.state.personalFirstName} {this.props.state.personalLastName}</p>
+            <p>{this.props.state.personalEmail}</p>
+            <p>{this.props.state.personalPhoneNumber}</p>
+          </div>
+          <div className='preview section'>
+            <h1>Education Information</h1>
+            <hr />
+            <p>{this.props.state.degree}</p>
+            <p>{this.props.state.school}</p>
+            <p>Started: {this.props.state.schoolStartDate} | Graduated: {this.props.state.schoolEndDate}</p>
+          </div>
+          <div className='preview section'>
+            <h1>Job Information</h1>
+            <hr />
+            <p>{this.props.state.companyName}</p>
+            <p>{this.props.state.jobTitle}</p>
+            <p>{this.props.state.jobDesc}</p>
+            <p>Started: {this.props.state.jobStart} | Left: {this.props.state.jobEnd}</p>
+          </div>
+        </div>
     )
   }
 
